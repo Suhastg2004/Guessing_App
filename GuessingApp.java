08576c9adb4c44660bc89c80e137e392a99330d3
Suhastg2004 <suhastg263399@gmail.com>
2026-02-23 11:35:55 +0530

    Initialized GuessingApp configuration with variabels and methods

---
+++ GuessingApp.java
@@ -0,0 +1,18 @@
+/**
+ * GuessingApp - Use Case 1: Game Initialization
+ *
+ * This class serves as the application entry point.
+ * It initializes the game configuration and displays game rules.
+ *
+ * No user input or gameplay logic is implemented at this stage.
+ *
+ * @author Suhas T G
+ * @version 1.0
+ */
+public class GuessingApp{
+    public static void main(String[] args){
+        System.out.println("Welcome to the Guessing App");
+        GameConfig gameConfig = new GameConfig();
+        gameConfig.showRules();
+    }
+}
